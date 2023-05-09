87ba2badf923be2799c24cbfa826b1fd8f991722
YX <asdf@hotmail.com>
2023-05-10 07:47:51 +0800

    added small details reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -12,6 +12,11 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    # @flight = Flight.find(params[:flight_id]) # Look for flight records, not sure if params are correct
+    # @seat = @flight.seats.find_by(row: params[:row], column: params[:column]) # Look for seat records
+
+    @reservation = Reservation.new(user: current_user, seat: @seat) # creates new reservation based on current user + seat selection
+    redirect_to :root # or wherever the landing page is
   end
 
   def update
