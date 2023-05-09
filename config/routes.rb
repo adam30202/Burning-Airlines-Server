f67987910ac32f5591c21b1f799f4f6d74c6ef17
vworo <victorworobec@gmail.com>
2023-05-09 18:55:00 +1000

    Cleaned up setup routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'reservations/index'
-  get 'reservations/new'
-  get 'reservations/edit'
-  get 'reservations/show'
-  get 'reservations/create'
-  get 'reservations/update'
-  get 'reservations/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :reservations
 end
