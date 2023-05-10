999f05714ad169912fb733fabd4eb20a1367e84f
vworo <victorworobec@gmail.com>
2023-05-10 11:03:17 +1000

    Airplanes controllers and basic views

---
+++ app/controllers/airplanes_controller.rb
@@ -0,0 +1,40 @@
+class AirplanesController < ApplicationController
+  def index
+    @airplanes = Airplane.all
+  end
+
+  def new
+      @airplane = Airplane.new
+  end
+
+  def create
+      airplane = Airplane.create airplane_params
+      redirect_to airplane
+  end
+
+  def edit
+      @airplane = Airplane.find params[:id]
+  end
+
+  def update
+      airplane = Airplane.find params[:id]
+      airplane.update airplane_params
+      redirect_to airplane
+  end
+
+  def show
+      @airplane = Airplane.find params[:id]
+  end
+
+  def destroy
+      airplane = Airplane.find params[:id]
+      airplane.destroy
+      redirect_to airplanes_path
+  end
+
+  private
+
+  def airplane_params
+      params.require(:airplane).permit(:rows, :column)
+  end
+end
