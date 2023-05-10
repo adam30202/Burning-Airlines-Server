7804da413697532eda76862fa470ec7522a59cf7
Yosiyas7 <josiahdebessay@gmail.com>
2023-05-10 14:27:14 +1000

    Added more seed data and made changes to controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
   resources :users, :only => [:index, :new, :create]
+  resources :admins, :only => [:index, :new, :create]
   resources :reservations
   resources :airplanes
+<<<<<<< HEAD
   resources :flights
+=======
+  resources :login
+  resources :sessions
 
-  get '/login' => 'session#new'
-  post '/login' => 'session#create'
-  delete '/login' => 'session#destroy'
+  get '/admin/signup' => 'admins#new'
+  post '/admin/signup' => 'admins#create'
+
+>>>>>>> 7206193 (Added more seed data and made changes to controllers)
+
 end
