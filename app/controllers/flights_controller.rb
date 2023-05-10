6e8858e9b9604b5f3a5afa6ca72d91b908023bc1
vworo <victorworobec@gmail.com>
2023-05-10 16:30:51 +1000

    Fixed seed data, merged 'fights_controller', Reservations controller/views

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,41 +1,50 @@
 class FlightsController < ApplicationController
-  def index
-    @flights = Flight.all
-    render json: @flights
-  end
+    before_action :set_flight, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_admin!, except: [:index, :show]
+    
+    def index
+        @flights = Flight.all
+        render json: @flights
+    end
 
-  def new
-      @flight = Flight.new
-  end
+    def new
+        @flight = Flight.new
+    end
 
-  def create
-      flight = Flight.create flight_params
-      redirect_to flight
-  end
+    def create
+        flight = Flight.create flight_params
+        redirect_to flight
+    end
 
-  def edit
-      @flight = Flight.find params[:id]
-  end
+    def edit
+        @flight = Flight.find params[:id]
+    end
 
-  def update
-      flight = Flight.find params[:id]
-      flight.update flight_params
-      redirect_to flight
-  end
+    def update
+        flight = Flight.find params[:id]
+        flight.update flight_params
+        redirect_to flight
+    end
 
-  def show
-      @flight = Flight.find params[:id]
-  end
+    def show
+        @flight = Flight.find params[:id]
+    end
 
-  def destroy
-      flight = Flight.find params[:id]
-      flight.destroy
-      redirect_to flights_path
-  end
+    def destroy
+        flight = Flight.find params[:id]
+        flight.destroy
+        redirect_to flights_path
+    end
 
-  private
+    private
 
-  def flight_params
-      params.require(:flight).permit(:rows, :column)
-  end
+    def flight_params
+        params.require(:flight).permit(:rows, :column)
+    end
+
+    def authenticate_admin!
+        unless current_user && current_user.admin?
+        redirect_to root_path, alert: "You must be an admin to access this page."
+        end
+    end
 end
