41bcb0721c5bc2db1ee150379850b8d64315cde2
vworo <victorworobec@gmail.com>
2023-05-10 11:25:51 +1000

    Fixed routes file clash

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   root :to => 'pages#home'
   resources :users, :only => [:index, :new, :create]
   resources :reservations
-<<<<<<< HEAD
   resources :airplanes
-=======
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy'
->>>>>>> 87ba2badf923be2799c24cbfa826b1fd8f991722
 end
