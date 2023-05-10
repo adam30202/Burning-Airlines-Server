92bb90754a73f6c96bde7b28b590480026b45427
Yosiyas7 <josiahdebessay@gmail.com>
2023-05-10 14:22:44 +1000

    Added more seed data and made changes to controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     has_many :reservations
     has_many :flights, :through => :reservations 
+    enum role: [:user, :admin]
+      
+    def admin?
+        role == "admin"
+    end
+      
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,10 +25,6 @@ ActiveRecord::Schema.define(version: 2023_05_10_014133) do
 
   create_table "flights", force: :cascade do |t|
     t.integer "airplane_id"
-    t.text "to"
-    t.text "from"
-    t.text "date"
-    t.integer "seats"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -44,6 +40,7 @@ ActiveRecord::Schema.define(version: 2023_05_10_014133) do
     t.text "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.string "role"
   end
 
